20a09502db7b9bad9ae4c7aa68cf66d0cdaaeb90
Elrojizo17 <alejandroescobard85@gmail.com>
2025-11-25 21:47:13 -0500

    Frontend Porfavor

--- frontend/src/pages/Dishes.jsx
+++ frontend/src/pages/Dishes.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import DishCard from "../components/DishCard.jsx";
+import DishCard from "./components/DishCard.jsx";
 
 export default function Dishes() {
     const [dishes, setDishes] = useState([]);
